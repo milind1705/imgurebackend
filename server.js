d1d8936a7bb6db72a95006dfe33e8d09c3b8a361
unknown <sois.milindsir@gmail.com>
2021-05-21 15:43:47 +0530

    storage use as mongodb

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const multer = require('multer');
 const path = require('path')
 const User = require('./models/model')
 const auth = require('./models/auth')
+var util = require('util')
+var GridFsStorage = require('multer-gridfs-storage')
+//const url = 
 const app = express();
 
 app.use(express.json())
@@ -14,14 +17,12 @@ mongoose.connect('mongodb://localhost:27017/imguser',{
     useNewUrlParser:true,
     useUnifiedTopology : true
 })
-var storage = multer.diskStorage({
-    destination: function (req, res , cb){
-        cb(null, 'uploads');
-    },
-    filename: function (req, file, cb){
-        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
-    }
-})
+var storage = new GridFsStorage({db:mongoose.connection})
+    
+//     filename: function (req, file, cb){
+//         cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+//     }
+// })
 var Upload = multer({storage:storage})
 
 app.set('view engine', 'hbs')
@@ -52,7 +53,11 @@ res.render('index')
 app.post('/imgure', Upload.single('image'), (req, res, next) => {
     //const file = req.file;
     res.send(req.file)
-
+    next();
+})
+app.get('/imgure',(req,res) => {
+    const imgUser = imguser.find()
+    res.redirect('/imgure',{User: imgUser})
 })
 
 app.listen(3000, (req, res) => {
